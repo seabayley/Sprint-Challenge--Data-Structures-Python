5a17cb73b5725130f4c38befbef22320a0051679
Christopher Bailey <seabayley@outlook.com>
2020-05-15 10:16:23 -0500

    Added names.py and stretch

--- names/names.py
+++ names/names.py
@@ -12,15 +12,23 @@ f.close()
 
 duplicates = []  # Return the list of duplicates in this data structure
 
-# Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# duplicates = [i for i in names_1 if i in names_2]
+
+# Gets all duplicates from each list and across lists in under 100th of a second. Wording is vague and doesn't specify
+# if you should show ALL duplicates or only elements duplicated across lists. Each individual list has
+# duplicates that aren't being reported in the starter code.
+last_value = ""
+for i, v in enumerate(sorted(names_1 + names_2)):
+    if last_value == v:
+        duplicates.append(v)
+    last_value = v
+
+
+# duplicates = list(set(names_1) & set(names_2))  # Stretch response
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
