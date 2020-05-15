dd2fba1083061f16204c1b4621e038cfd40eec68
Christopher Bailey <seabayley@outlook.com>
2020-05-15 10:21:07 -0500

    Added recursive reverse method

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -12,6 +12,7 @@ class Node:
     def set_next(self, new_next):
         self.next_node = new_next
 
+
 class LinkedList:
     def __init__(self):
         self.head = None
@@ -39,4 +40,10 @@ class LinkedList:
         return False
 
     def reverse_list(self, node, prev):
-        pass
+        if node is None:
+            return
+        if node.next_node != None:
+            self.reverse_list(node.next_node, node)
+        else:
+            self.head = node
+        node.next_node = prev
